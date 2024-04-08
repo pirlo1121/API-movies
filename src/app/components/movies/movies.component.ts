5de18b5ecd105ba0e9c2914746ac71ae29be0ec0
Maicol bautista <pirlo1121s@gmail.com>
2024-04-07 22:06:54 -0500

    {+} observable and rxjs

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription, debounceTime, distinct, filter, from, fromEvent, map, switchMap, tap } from 'rxjs';
+import { Movie } from 'src/app/interfaces/movie';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -6,22 +8,31 @@ import { MovieService } from 'src/app/services/movie.service';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
-  movies: any[] =[];
+export class MoviesComponent implements OnInit, OnDestroy {
+  movies: Movie[] =[];
+  @ViewChild('movieSearchInput', {static: true}) movieSearchInput! : ElementRef;
+  movieSubscription!: Subscription;
 
   constructor(private moviesServices: MovieService){}
 
   ngOnInit(): void {
-
-  }
-
-  getMovies(event: Event){
-    const searchTerm = (event.target as HTMLInputElement).value
-    console.log(searchTerm);
-    this.moviesServices.getMovies(searchTerm).subscribe(movies =>{
-      console.log(movies);
+    this.movieSubscription = fromEvent<Event>(this.movieSearchInput.nativeElement, 'keyup').pipe(
+      map((event: Event) => {
+        const searchTerm = (event.target as HTMLInputElement).value;
+        return searchTerm;
+      }),
+      filter((searchTerm: string)=> searchTerm.length > 3),
+      tap((searchTerm: string)=> console.log(searchTerm)),
+      debounceTime(500),
+      distinct(),
+      switchMap((searchTerm: string)=> this.moviesServices.getMovies(searchTerm))
+    ).subscribe((movies: Movie[]) =>{
       this.movies = movies !== undefined ? movies : [];
-    });
+
+    })
+  }
+  ngOnDestroy(): void {
+    this.movieSubscription.unsubscribe();
   }
 
 }
